35ea50fde9009be7504eab8755880e79e875cd8c
Andrey Zakharenkov <zahaand@gmail.com>
2022-12-29 17:33:38 +0300

    add Swagger

--- src/main/java/group/ship/blackshipstore/controller/OrderController.java
+++ src/main/java/group/ship/blackshipstore/controller/OrderController.java
@@ -2,11 +2,14 @@ package group.ship.blackshipstore.controller;
 
 import group.ship.blackshipstore.entity.Order;
 import group.ship.blackshipstore.sevices.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @RequestMapping("/order")
+@Tag(name = "Order Controller", description = "Controller to get Pirate Orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -15,22 +18,26 @@ public class OrderController {
         this.orderService = orderService;
     }
     @GetMapping("/{id}")
+    @Operation(summary = "Get all Pirate Orders", description = "Return list of all Orders by Pirate id")
     public List<Order> getAllOrdersByPirateId(@PathVariable Integer id) {
         return orderService.getAllOrdersByPirateId(id);
     }
 
-    @GetMapping("/unique/{id}")
+    @GetMapping("unique/{id}")
+    @Operation(summary = "Get last Pirate Order", description = "Return last Order by Pirate id")
     public Order getLastOrderByPirateId(@PathVariable Integer id) {
         return orderService.getLastOrderByPirateId(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Mark Order completed", description = "Return Order that has been marked as completed by id")
     public Order markOrderAsCompleted(@PathVariable Integer id) {
         return orderService.markOrderAsCompleted(id);
     }
 
     @PostMapping("/{id}")
-    public void addOrder(@PathVariable("id") Integer id) {
+    @Operation(summary = "Add Order to Pirate", description = "Add Order to Pirate by Pirate id")
+    public void addOrder(@PathVariable Integer id) {
         orderService.addOrderByPirateId(id);
     }
 }
