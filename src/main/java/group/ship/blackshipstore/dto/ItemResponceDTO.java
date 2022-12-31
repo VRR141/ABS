c5ef5247ce7d48da1ba91d5e2cc3e4420cb81fad
Konstantin Artamonov <artkonstantin1987@gmail.com>
2022-12-31 16:12:07 +0300

    added dto

---
+++ src/main/java/group/ship/blackshipstore/dto/ItemResponceDTO.java
@@ -0,0 +1,12 @@
+package group.ship.blackshipstore.dto;
+
+public class ItemResponceDTO {
+    private String itemName;
+    public ItemResponceDTO(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+}
