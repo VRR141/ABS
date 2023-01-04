7ddba4fce193a4db363908700f53336a62a0641c
Gleb Buryak <godri5@yandex.ru>
2023-01-04 22:49:55 +0300

    Refactor ItemController and Service (Indents, mapping) Add method findById

--- src/main/java/group/ship/blackshipstore/sevices/ItemService.java
+++ src/main/java/group/ship/blackshipstore/sevices/ItemService.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @Service
 @Transactional(readOnly = true)
 public class ItemService {
+
     private final ItemRepository itemRepository;
 
     @Autowired
@@ -36,5 +37,9 @@ public class ItemService {
         return itemRepository.findAllByCategoryId(categoryId).stream()
                 .map(itemDtoFunction)
                 .collect(Collectors.toList());
+    }
+
+    public ItemResponseDto findById(Long id) {
+        return itemRepository.findById(id).map(itemDtoFunction).orElse(null);
     }
 }
