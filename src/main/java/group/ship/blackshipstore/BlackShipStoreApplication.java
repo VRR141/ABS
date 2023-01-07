3f4cb14b8dedfb54ae479d59bce94012fc241930
Vladislav <v.r.ryazantsev@gmail.com>
2023-01-07 15:06:14 +0300

    add spring cache

--- src/main/java/group/ship/blackshipstore/BlackShipStoreApplication.java
+++ src/main/java/group/ship/blackshipstore/BlackShipStoreApplication.java
@@ -3,12 +3,7 @@ package group.ship.blackshipstore;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cache.Cache2kBuilderCustomizer;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
