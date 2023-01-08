c4ae1956d34095d654cf504c21ad348d689cb24a
Gleb Buryak <godri5@yandex.ru>
2023-01-08 14:07:43 +0300

    Add @Repository

--- src/main/java/group/ship/blackshipstore/repositories/StatusRepository.java
+++ src/main/java/group/ship/blackshipstore/repositories/StatusRepository.java
@@ -1,6 +1,8 @@
 package group.ship.blackshipstore.repositories;
 
 import group.ship.blackshipstore.entity.Status;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StatusRepository extends BaseRepository<Status> {
 }
