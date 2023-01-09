6af32005e4fd98516c0afa7778096938b1dd1df4
Godri5 <106228542+Godri5@users.noreply.github.com>
2023-01-09 21:10:52 +0300

    Update ArticleResponseDto.java

--- src/main/java/group/ship/blackshipstore/dto/response/ArticleResponseDto.java
+++ src/main/java/group/ship/blackshipstore/dto/response/ArticleResponseDto.java
@@ -6,22 +6,12 @@ public class ArticleResponseDto {
 
     private int price;
 
-    private int amount;
-
     public int getPrice() {
         return price;
     }
 
     public void setPrice(int price) {
         this.price = price;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
     }
 
     public long getId() {
